7dee91cd1c70eb81ba97afc4c0af17a58388b14a
Stanislav Atanasov <lucifer@anavaro.com>
2014-06-25 13:23:02 +0300

    Some changes

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 birthdaycontrol
 ===============
 
-PhpBB 3.1 Birth Day extension
+PhpBB 3.1 Birth Day controll extension
+
+This little tool allows to control user registration by age. It allows user age to be shown in profile and topic rows.
---
+++ ext.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*
+* Birthday Control extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 Lucifer <http://www.anavaro.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace anavaro\birthdacontrol;
+
+/**
+* Extension class for board announcements
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
