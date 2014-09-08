65d973283f51202d303950a6cd1d525896882d76
Stanislav Atanasov <lucifer@anavaro.com>
2014-09-09 00:29:12 +0300

    Debuging tests

--- tests/functional/birthdaycontrol_register_test.php
+++ tests/functional/birthdaycontrol_register_test.php
@@ -24,7 +24,7 @@ class birthdaycontrol_register_test extends birthdaycontrol_base
 		
 		$crawler = self::request('GET', 'ucp.php?mode=register&sid='. $this->sid);
 		
-		$this->assertContains('BIRTHDAY', $crawler->text());
+		$this->assertContainsLang('BIRTHDAY', $crawler->text());
 		
 		$form = $crawler->selectButton($this->lang('AGREE'))->form();
 		
