0871ac9789e40ad9c7d3936f73be5684f272447a
Stanislav Atanasov <lucifer@anavaro.com>
2014-09-09 13:35:53 +0300

    Remove debug

--- event/acplistener.php
+++ event/acplistener.php
@@ -67,7 +67,7 @@ class acplistener implements EventSubscriberInterface
 			$display_vars['vars'] = phpbb_insert_config_array($display_vars['vars'], $my_config_vars, array('after' =>'warnings_expire_days'));
 			// Update the display_vars  event with the new array
 			$event['display_vars'] = array('title' => $display_vars['title'], 'vars' => $display_vars['vars']);
-			$this->var_display($display_vars['vars']);
+			//$this->var_display($display_vars['vars']);
 		}
 
 	}
