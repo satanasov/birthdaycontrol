fcae2bca5538af1e661382a08a449f29630a9813
Stanislav Atanasov <lucifer@anavaro.com>
2014-09-10 14:49:03 +0300

    Testing

--- tests/functional/birthdaycontrol_users_test.php
+++ tests/functional/birthdaycontrol_users_test.php
@@ -19,7 +19,7 @@ class birthdaycontrol_users_test extends birthdaycontrol_base
 	{
 		$this->login();
 		// Test creating topic and post to test
-		$post = $this->create_topic(2, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
+		$post = $this->create_topic(3, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
 		//$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
 		//$post2 = $this->create_post(2, $post['topic_id'], 'Re: Test Topic 1', 'This is a test [b]post[/b] posted by the testing framework.');
 		//$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
