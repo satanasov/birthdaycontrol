13c038a6863b4a4c3e0bb6b47a11be725fe2b5be
Stanislav Atanasov <lucifer@anavaro.com>
2014-09-10 13:30:54 +0300

    Testing ... without create new topic

--- tests/functional/birthdaycontrol_users_test.php
+++ tests/functional/birthdaycontrol_users_test.php
@@ -15,32 +15,18 @@ namespace anavaro\birthdaycontrol\tests\functional;
 */
 class birthdaycontrol_users_test extends birthdaycontrol_base
 {
-	public function test_install_users()
-	{
-		$this->create_user('testuser1');
-		$this->add_user_group('NEWLY_REGISTERED', array('testuser1'));
-		
-		$this->login();
-		// Test creating topic and post to test
-		$post = $this->create_topic(2, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
-		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
-		$post2 = $this->create_post(2, $post['topic_id'], 'Re: Test Topic 1', 'This is a test [b]post[/b] posted by the testing framework.');
-		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
-		
-		$this->assertContains('topic posted by', $crawler->filter('html')->text());
-		
-		$this->logout();
-	}
-	
 	public function test_viewbday_in_topic()
 	{
+	
+		$this->create_user('testuser1');
+		$this->add_user_group('NEWLY_REGISTERED', array('testuser1'));
 		$this->set_show_birthday($this->get_user_id('admin'), 2);
 		
 		$this->login('testuser1');
 		$this->add_lang('ucp');
 		$this->add_lang('common');
 		
-		$crawler = self::request('GET', 'viewtopic.php?t=' . $this->get_topic_id('Test Topic 1') . '&sid=' . $this->sid);
+		$crawler = self::request('GET', 'viewtopic.php?t=1&sid=' . $this->sid);
 		$this->assertContains('This is a test topic posted by the testing framework.', $crawler->filter('html')->text());
 		
 		$this->assertContainsLang('AGE', $crawler->filter('#bc_age')->text());
