c3b0c21ac3ed5c0a8b5b893f3a6b188cf5f840d7
Stanislav Atanasov <lucifer@anavaro.com>
2014-09-10 13:26:17 +0300

    Testing 503 error

--- tests/functional/birthdaycontrol_users_test.php
+++ tests/functional/birthdaycontrol_users_test.php
@@ -22,12 +22,12 @@ class birthdaycontrol_users_test extends birthdaycontrol_base
 		
 		$this->login();
 		// Test creating topic and post to test
-		$this->post = $this->create_topic(2, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
-		$crawler = self::request('GET', "viewtopic.php?t={$this->post['topic_id']}&sid={$this->sid}");
-		
-		$this->assertContains('This is a test topic posted by the testing framework.', $crawler->filter('html')->text());
-		$post2 = $this->create_post(2, $this->post['topic_id'], 'Re: Test Topic 1', 'This is a test [b]post[/b] posted by the testing framework.');
+		$post = $this->create_topic(2, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
+		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
+		$post2 = $this->create_post(2, $post['topic_id'], 'Re: Test Topic 1', 'This is a test [b]post[/b] posted by the testing framework.');
 		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
+		
+		$this->assertContains('topic posted by', $crawler->filter('html')->text());
 		
 		$this->logout();
 	}
