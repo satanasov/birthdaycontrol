ac3fdd612b3e813f81f6f4231c813dae0d9d2e1f
Stanislav Atanasov <lucifer@anavaro.com>
2015-04-02 22:16:28 +0300

    Test invalid date

--- tests/functional/birthdaycontrol_register_test.php
+++ tests/functional/birthdaycontrol_register_test.php
@@ -17,12 +17,26 @@ class birthdaycontrol_register_test extends birthdaycontrol_base
 {
 	public function test_register_agreement_invalid_date()
 	{
-		$this->force_allow_birthday();
-		$this->require_birthday();
-		//firstly set all
-		$this->set_birthday_min_age(18);
-		
 		$this->add_lang_ext('anavaro/birthdaycontrol', 'ucp_lang');
+		$this->add_lang('common');
+		
+		$this->login();
+		$this->admin_login();
+		// Change option
+		$crawler = self::request('GET', 'adm/index.php?i=acp_board&mode=settings&sid=' . $this->sid);
+		$form = $crawler->selectButton('submit')->form();
+		$form->setValues(array(
+			'config[allow_birthdays]'	=> 1,
+			'config[birthday_require]'	=> 1,
+			'config[birthday_min_age]'	=> 18,
+		));
+		$crawler = self::submit($form);
+		// Should be updated
+		$this->assertContainsLang('CONFIG_UPDATED', $crawler->text());
+		$this->logout();
+		$this->logout();
+		
+		// test
 		$this->add_lang('ucp');
 		
 		$crawler = self::request('GET', 'ucp.php?mode=register&sid='. $this->sid);
@@ -38,9 +52,22 @@ class birthdaycontrol_register_test extends birthdaycontrol_base
 		$crawler = self::submit($form);
 		
 		$this->assertContains('You are not old enough to register to this board.', $crawler->text());
-		
-		$this->force_allow_birthday(0);
-		$this->require_birthday(0);
+
+		$this->login();
+		$this->admin_login();
+		// Change option
+		$crawler = self::request('GET', 'adm/index.php?i=acp_board&mode=settings&sid=' . $this->sid);
+		$form = $crawler->selectButton('submit')->form();
+		$form->setValues(array(
+			'config[allow_birthdays]'	=> 0,
+			'config[birthday_require]'	=> 0,
+			'config[birthday_min_age]'	=> 0,
+		));
+		$crawler = self::submit($form);
+		// Should be updated
+		$this->assertContainsLang('CONFIG_UPDATED', $crawler->text());
+		$this->logout();
+		$this->logout();
 	}/*
 	public function test_register_agreement_no_date()
 	{
