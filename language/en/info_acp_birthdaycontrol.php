7e4883dc48372b0c6190c693549f63183f820262
Raphaël M. <Galixte@users.noreply.github.com>
2015-06-22 19:36:25 +0200

    Minor update of ENGLISH language for Birthday Control extension

--- language/en/info_acp_birthdaycontrol.php
+++ language/en/info_acp_birthdaycontrol.php
@@ -24,9 +24,9 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'BIRTHDAY_CONTROL'	=> 'Control birthday requirements',
 	'BIRTHDAY_REQUIRE'	=> 'Require birthday',
-	'BIRTHDAY_REQUIRE_EXPLAIN'	=> 'Require user to enter age to register here',
+	'BIRTHDAY_REQUIRE_EXPLAIN'	=> 'Require user to enter age to register here.',
 	'BIRTHDAY_MIN_AGE'	=> 'Minimum age',
-	'BIRTHDAY_MIN_AGE_EXPLAIN'	=> 'Require minimum age to register in this board',
+	'BIRTHDAY_MIN_AGE_EXPLAIN'	=> 'Require minimum age to register in this board.',
 	'BIRTHDAY_SHOW_POST'	=> 'Show age in posts',
 	'BIRTHDAY_SHOW_POST_EXPLAIN'	=> 'Show user age in small profile in postview.',
 
